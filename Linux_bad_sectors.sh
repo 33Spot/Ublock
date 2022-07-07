ab7ff77673a13a16f05b9615610fcb7b7c7e61ec
33Spot <50510349+33Spot@users.noreply.github.com>
2022-07-07 23:15:40 +0200

    Update Linux_bad_sectors.sh

--- Linux_bad_sectors.sh
+++ Linux_bad_sectors.sh
@@ -10,4 +10,109 @@ sudo e2fsck -l bad_sectors.txt /dev/sda1
 # For other file systems (such as FAT32), you can use fsck.
 sudo fsck -l bad_sectors.txt /dev/sda1
 
+readarray -t lines < <(lsblk --nodeps -no name,serial,size | grep "sd")
+
+
+# Prompt the user to select one of the lines.
+echo "Please select a drive:"
+select choice in "${lines[@]}"; do
+  [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }
+  break # valid choice was made; exit prompt.
+done
+
+# Split the chosen line into ID and serial number.
+read -r id sn unused <<<"$choice"
+
+echo "id: [$id]; s/n: [$sn]"
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Dynamic Menu Function
+createmenu () {
+    select selected_option; do # in "$@" is the default
+        if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $(($#)) ]; then
+            break;
+        else
+            echo "Please make a vaild selection (1-$#)."
+        fi
+    done
+}
+
+declare -a drives=();
+# Load Menu by Line of Returned Command
+mapfile -t drives < <(lsblk --nodeps -o name,serial,size | grep "sd");
+# Display Menu and Prompt for Input
+echo "Available Drives (Please select one):";
+createmenu "${drives[@]}"
+# Split Selected Option into Array and Display
+drive=($(echo "${selected_option}"));
+echo "Drive Id: ${drive[0]}";
+echo "Serial Number: ${drive[1]}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# define an array
+declare -a obj
+
+# capture the current IFS
+cIFS=$IFS
+
+# change IFS to something else
+IFS=:
+
+# assign & format output from lsblk 
+obj=( $(lsblk --nodeps --no name,serial,size) )
+
+# generate a menu system
+select item from ${obj[@]}; do
+  if  [ -n ${item} ]; then
+    echo "Invalid selection"
+    continue
+  else
+    selection=${item}
+    break
+  fi
+done
+
+# reset the IFS
+IFS=${cIFS}
+
+
+
+
+
+
+
+# without readarray for the first script
+IFS=$'\n' read -d '' -ra lines < <(lsblk --nodeps -no name,serial,size | grep "sd")
+
+
+
+
+
+
+
+
 
