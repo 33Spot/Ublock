ad20432f8c0adf0b61a79d60eb9860398a2703a1
33Spot <50510349+33Spot@users.noreply.github.com>
2022-07-14 01:51:42 +0200

    Update GPD_pocket2_Linux_Screen_FIX.sh

--- GPD_pocket2_Linux_Screen_FIX.sh
+++ GPD_pocket2_Linux_Screen_FIX.sh
@@ -5,9 +5,9 @@ xrandr -o right
 
 # SET DISPLAY TO A SMALLER RESOLUTION
 
-xrandr --newmode "600x950_60.00"   46.75 600 640 696 792 950 953 963 986 -hsync +vsync
-xrandr --addmode eDP-1 "600x950_60.00"
-xrandr --output eDP-1 --mode 600x950_60.00
+#xrandr --newmode "600x950_60.00"   46.75 600 640 696 792 950 953 963 986 -hsync +vsync
+#xrandr --addmode eDP-1 "600x950_60.00"
+#xrandr --output eDP-1 --mode 600x950_60.00
 
 # ROTATE TOUCHSCREEN
 # Goodix Capacitive Touchscreen id=15
