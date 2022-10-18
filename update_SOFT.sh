c14d67f8e6498cefd18e662a8153aab48da78870
33Spot <50510349+33Spot@users.noreply.github.com>
2022-10-19 01:10:46 +0200

    Add files via upload

---
+++ update_SOFT.sh
@@ -0,0 +1,73 @@
+
+
+
+
+gdebi=dpkg\ \-\-force-all\ \-i
+
+
+
+url=https://download.xnview.com/XnViewMP-linux-x64.deb
+debpackage=XnViewMP-linux-x64.deb
+package=./$debpackage
+sudo gdebi $package
+if [ ! -f $package ]; then
+(
+wget $url
+sudo $gdebi $package
+)
+fi
+
+if [ -f $package ]; then
+(
+rm -f $package
+)
+fi
+
+
+
+
+
+
+
+
+
+
+#url=https://github.com$(wget -qO- 'https://github.com/FreeTubeApp/FreeTube/releases/latest' | grep "amd64.deb" | sed -e "/span/d" -e "/FreetubeApp/,/deb/p" | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D' | head -n 1)
+url_p=$(./xidel -e '(//@href, //@src)/resolve-uri(.)' https://github\.com/FreeTubeApp/FreeTube/releases/latest | grep "releases/expanded" | head -n 1)
+url=https://github.com$(wget -qO- $url_p | grep "amd64.deb" | sed -e "/span/d" -e "/FreetubeApp/,/deb/p" | sed -n '/"/!{/\n/{P;b}};s/"/\n/g;D' | head -n 1)
+rm -f index.html
+debpackage=${url##*/}
+package=./$debpackage
+sudo gdebi $package
+if [ ! -f $package ]; then
+(
+wget $url
+sudo $gdebi $package
+)
+fi
+
+if [ -f $package ]; then
+(
+rm -f $package
+)
+fi
+
+
+
+
+
+
+
+
+
+
+#wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+#echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+#sudo apt update
+#sudo apt install xsel
+
+echo done!
+#read r;
+
+
+
